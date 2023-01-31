f8906b8f4d38d961924ec6683fa23c5f485db238
Maxi Giagnorio <maxigiagnorio@gmail.com>
2023-01-31 20:08:43 -0300

    add dotenv index.js

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+require("dotenv").config();
 const { PORT } = process.env;
 
 // Syncing all the models at once.
