abc197c7b6f33a9f5520d04f554cc06c6d6a1073
Maxi Giagnorio <maxigiagnorio@gmail.com>
2023-01-31 20:35:43 -0300

    djqwhrwq

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://henry-project-food-ew90p6v7e-mxgiagnorio.vercel.app"
+    "https://henry-project-food.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
